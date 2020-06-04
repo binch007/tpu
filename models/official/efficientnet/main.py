2c500c50e1ce0db21c18ef3259583dbd10c88a89
Min Hye Chang <binch007@gmial.com>
2020-06-04 16:44:24 +0900

    ADD set gpu

--- models/official/efficientnet/main.py
+++ models/official/efficientnet/main.py
@@ -44,6 +44,15 @@ import ptvsd
 ptvsd.enable_attach()
 ptvsd.wait_for_attach()
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # 텐서플로가 첫 번째 GPU만 사용하도록 제한
+    try:
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    except RuntimeError as e:
+    # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
+        print(e)
+
 
 FLAGS = flags.FLAGS
 
